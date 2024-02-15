3f3d48bbf939b36d9b908d0df939e83addb50c53
iamvysh <vyshnavthaithottathil@gmail.com>
2024-02-15 19:38:02 +0530

    changed mdb opath

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Routes,Route } from "react-router-dom"
 import Landing from "./Pages/Landing"
 import Product from "./Pages/Product"
 import Cart from "./Pages/Cart"
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import { BrowserRouter } from 'react-router-dom'
 import { persistor, store } from './redux/store.js'
 import { Provider } from 'react-redux'
