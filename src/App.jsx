231615f04630095b0efceacc5e85e36c42286f72
iamvysh <vyshnavthaithottathil@gmail.com>
2024-02-16 10:17:26 +0530

    removed fontawesome

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Landing from "./Pages/Landing"
 import Product from "./Pages/Product"
 import Cart from "./Pages/Cart"
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 function App() {
